21d8dd1ac3be2ade5d780190afd052f848a06816
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-04-12 18:59:46 -0300

    Create index.jsx

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from './style';
+
+export function Details () {
+  return (
+    <Container>
+      <h1>Hello World</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </Container>
+  )
+}
