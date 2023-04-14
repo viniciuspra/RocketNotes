37401ee06e88e84c1b10ecea2cec4495f4f3488d
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-04-14 17:16:07 -0300

    Update index.jsx

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,13 @@
 import { Container } from './style';
+import { Button } from '../../components/button';
 
 export function Details () {
   return (
     <Container>
       <h1>Hello World</h1>
       <p>Lorem ipsum dolor sit amet.</p>
+
+      <Button title="Voltar" />
     </Container>
   )
 };
