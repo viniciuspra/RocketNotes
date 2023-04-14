a80509af80fd1355acb8e2613bb2a106ec412c32
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-04-14 19:15:42 -0300

    Create Comp. Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,22 @@
+import { RiShutDownLine } from "react-icons/ri";
+import { Container, Profile, Logout} from "./styles";
+
+export function Header() {
+  return (
+    <Container>
+      <Profile>
+        <img src="https://github.com/viniciuspra.png" alt="Foto do usuario" />
+
+        <div>
+          <span>Bem-Vindo</span>
+          <strong>Vinicius Cascaes</strong>
+        </div>
+      </Profile>
+
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
+
+    </Container>
+  )
+}
