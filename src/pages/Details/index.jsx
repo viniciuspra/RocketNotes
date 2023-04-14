48ef8b34b24440036562bfbede08a5527cd4c103
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-04-14 19:16:29 -0300

    Update index.jsx

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,26 @@
-import { Container } from './style';
-import { Button } from '../../components/button';
+import { Container, Links } from './styles';
+import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
+import { Button } from '../../components/Button';
+import { Tags } from '../../components/tags';
+
 
 export function Details () {
   return (
     <Container>
-      <h1>Hello World</h1>
-      <p>Lorem ipsum dolor sit amet.</p>
+      <Header /> 
+
+      <Section title="Links úteis">
+        <Links>
+          <li><a href="#" target='_blank'>https://github.com/Viniciuspra</a></li>
+          <li><a href="#" target='_blank'>https://github.com/Viniciuspra</a></li>
+        </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tags title="Express"/>
+        <Tags title="NodeJs"/>
+      </Section>
 
       <Button title="Voltar" />
     </Container>
