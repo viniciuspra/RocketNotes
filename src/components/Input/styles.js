e810b9cc212a841423f13bd3cf177a30025bbd0d
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-04-15 16:39:58 -0300

    Create styles.js

---
+++ src/components/Input/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+  color: ${({ theme }) => theme.COLORS.GRAY_300};
+
+  margin-bottom: 8px;
+  border-radius: 10px;
+
+  > svg {
+    margin: 0 16px;
+  }
+
+  > input {
+    width: 100%;
+    height: 56px;
+
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    background: transparent;
+    border: 0;
+
+    &::placeholder {
+      color: ${({ theme }) => theme.COLORS.GRAY_300};
+    }
+  }
+`
