7af48c51671be8f15fbb5cbf16167ef84e3f45f0
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-04-18 16:32:10 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global'
 import theme from './styles/theme';
-import { Profile } from "./pages/Profile"
+import { NewNote } from "./pages/NewNote"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Profile />
+      <NewNote />
     </ThemeProvider>
   </React.StrictMode>
 )
