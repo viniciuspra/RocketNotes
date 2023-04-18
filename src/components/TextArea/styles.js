bbc90b96504d1f479a2c9d4fccb49e46b68174ec
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-04-18 18:18:29 -0300

    add TextArea component

---
+++ src/components/TextArea/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.textarea`
+  width: 100%;
+  height: 150px;
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+  color: ${({ theme }) => theme.COLORS.WHITE};
+
+  margin-bottom: 8px;
+  padding: 16px;
+
+  resize: none;
+  border: none;
+  border-radius: 10px;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.COLORS.GRAY_300};
+  }
+`
