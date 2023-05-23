a4a6100d73a86de2dd8bd2545fcc47b2ce43ecf7
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-05-23 16:04:09 -0300

    Fix redirection after login

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import GlobalStyle from './styles/global'
-import theme from './styles/theme';
+import { ThemeProvider } from 'styled-components'
+
+import { AuthProvider } from './hooks/auth'
+
 import { Routes } from "./routes"
+
+import theme from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react"
+
 import { Container, Form, Background } from "./styles"
-import { FiMail, FiLock} from "react-icons/fi"
+import { FiMail, FiLock } from "react-icons/fi"
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
 import { Link } from "react-router-dom"
 
+import { useAuth } from "../../hooks/auth"
 
 export function SignIn() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const { signIn } = useAuth()
+
+  function handleSignIn() {
+    signIn({email, password})
+  }
+
   return (
     <Container>
       <Form>
@@ -14,15 +26,25 @@ export function SignIn() {
 
         <h2>Faça seu login</h2>
 
-        <Input placeholder="E-mail" type="text" icon={FiMail}/> 
-        <Input placeholder="Senha" type="password" icon={FiLock}/> 
+        <Input 
+          placeholder="E-mail" 
+          type="text" icon={FiMail} 
+          onChange={(e) => setEmail(e.target.value)} 
+          />
 
-        <Button title="Entrar"/>
-        
+        <Input 
+          placeholder="Senha" 
+          type="password" 
+          icon={FiLock} 
+          onChange={(e) => setPassword(e.target.value)} 
+          />
+
+        <Button title="Entrar" onClick={handleSignIn} />
+
         <Link to="/register">Criar Conta</Link>
       </Form>
 
-      <Background/>
+      <Background />
 
     </Container>
   )
