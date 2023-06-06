805c692dc0cc6d6fc86ab0143f69a63b41ade017
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-06-06 18:54:18 -0300

    Implement note details loading

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,56 +1,99 @@
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 import { Container, Links, Content } from './styles';
-import { Header } from '../../components/Header';
-import { ButtonText } from '../../components/ButtonText';
-import { Section } from '../../components/Section';
+
+import { api } from '../../services/api';
+
+import { v4 as uuidv4 } from 'uuid';
+
 import { Tag } from '../../components/Tag';
+import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
+import { Section } from '../../components/Section';
+import { ButtonText } from '../../components/ButtonText';
 
 
-export function Details () {
+export function Details() {
+  const [data, setData] = useState(null)
+
+  const params = useParams()
+
+  const navigate = useNavigate()
+
+  function handleBack() {
+    navigate(-1)
+  }
+
+  async function handleRemove() {
+    const confirm = window.confirm('Tem certeza de que deseja excluir esta nota?')
+
+    if (confirm) {
+      await api.delete(`/notes/${params.id}`)
+      navigate(-1)
+    }
+  }
+
+  useEffect(() => {
+    async function fetchNote() {
+      const response = await api.get(`/notes/${params.id}`)
+      setData(response.data)
+    }
+
+    fetchNote()
+  }, [])
+
   return (
     <Container>
       <Header />
+      {
+        data &&
+        <main>
+          <Content>
+            <ButtonText
+             title="Excluir Nota" 
+             onClick={handleRemove}
+            />
 
-      <main>
-        <Content>
-          <ButtonText title="Excluir Nota" />
-
-          <h1>Introdução ao React</h1>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
-
-          <Section title="Links úteis">
-            <Links>
-              <li>
-                <a href="#" target="_blank">
-                  https://github.com/Viniciuspra
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_blank">
-                  https://github.com/Viniciuspra
-                </a>
-              </li>
-            </Links>
-          </Section>
-
-          <Section title="Marcadores">
-            <Tag title="Express" />
-            <Tag title="NodeJs" />
-          </Section>
-
-          <Button title="Voltar" />
-        </Content>
-      </main>
+            <h1>{data.title}</h1>
+            <p>
+              {data.description}
+            </p>
+            {
+              data.links &&
+              <Section title="Links úteis">
+                <Links>
+                  {
+                    data.links.map(link => (
+                      <li key={uuidv4()}>
+                        <a href={link.url} target="_blank">
+                          {link.url}
+                        </a>
+                      </li>
+                    ))
+                  }
+                </Links>
+              </Section>
+            }
+            {
+              data.tags &&
+              <Section title="Marcadores">
+                {
+                  data.tags.map(tag => (
+                    <Tag
+                      key={uuidv4()}
+                      title={tag.name}
+                    />
+                  ))
+                }
+              </Section>
+            }
+            <Button
+             title="Voltar" 
+             onClick={handleBack}
+            />
+          </Content>
+        </main>
+      }
     </Container>
   )
 };
